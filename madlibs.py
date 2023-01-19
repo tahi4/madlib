21b9b79cea32afb91e70ca94534bc27ba37f3a14
Muntaha Tahiat <107261746+tahi4@users.noreply.github.com>
2023-01-19 22:41:36 +0600

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -15,6 +15,6 @@ food = input("Food: ")
 
 # actual madlib, added f" " so i can use other input variables 
 
-madlib = f"Say '{food},' the photographer said as the camera flashed! {person} and I had gone to the {place} to get our photos taken today. The first photo we really wanted was a picture of us dressed as {animal} pretending to be a {professional}. When we saw the proofs of it, I was a bit {feeling} because it looked different than in my head. (I hadn't imagined so many {things1} behind us.) However, the second photo was exactly what I wanted. We both looked like {things2} wearing {clothing} and {verb_ing}--exactly what I had in mind!"
+madlib = f"Say '{food},' the photographer said as the camera flashed! {person} and I had gone to the {place} to get our photos taken today. The first photo we really wanted was a picture of us dressed as {animal} pretending to be a {professional}. When we saw the proofs of it, I was a bit {feeling} because it looked different than in my head. (I hadn't imagined so many {things1} behind us.) However, the second photo was exactly what I wanted. We both looked like {things2} wearing {clothing} and {verb_ing} --exactly what I had in mind!"
 
 print(madlib)
